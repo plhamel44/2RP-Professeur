200df06a1af3b43e8110f8c951e2be1c757d2642
Pierre-Luc Hamel <plhamel44@gmail.com>
2021-02-17 11:38:42 -0500

    Ajout du code de test

--- Professeur.js
+++ Professeur.js
@@ -26,3 +26,20 @@ class EmployeUniversiteProfesseur extends EmployeUniversite {
         this.coursEnseigne[this.coursEnseigne.length] = nouveauCours;
     }
 }
+
+employe = new EmployeUniversite('Pierre', 'Martel', 'E0004', 5);
+employe.augmenterAnciennete();
+console.log(employe.nomComplet());
+console.log(employe.anneesExperience);
+
+prof = new EmployeUniversiteProfesseur('Carmel', 'Simon', 'P0007', 4, 65000, []);
+prof.augmenterAnciennete();
+console.log(prof.nomComplet());
+console.log(prof.anneesExperience);
+console.log(prof.salaire);
+console.log(prof.coursEnseigne);
+
+prof.ajouterUnCours('2RP');
+console.log(prof.coursEnseigne);
+prof.ajouterUnCours('CG2');
+console.log(prof.coursEnseigne);
