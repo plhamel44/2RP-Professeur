0eae3f56104beb19695d3eb6b46ae00ad076e976
Pierre-Luc Hamel <plhamel44@gmail.com>
2021-02-17 11:24:14 -0500

    Création de la classe EmployeUniversite

---
+++ Professeur.js
@@ -0,0 +1,22 @@
+class EmployeUniversite {
+    constructor(nom, prenom, numeroEmploye, anneesExperience) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.numeroEmploye = numeroEmploye;
+        this.anneesExperience = anneesExperience;
+    }
+
+    augmenterAnciennete() {
+        this.anneesExperience++;
+    }
+
+    nomComplet() {
+        return this.prenom + ", " + this.nom;
+    }
+
+/*     information() {
+        return this.numeroEmploye + " - "
+            + this.nomComplet() +
+            " - Ancienneté: " + this.anneesExperience;
+    } */
+}
